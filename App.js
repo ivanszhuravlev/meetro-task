6f519f65965c476e0bf30c8451a7f5170cc0080d
Ivan <ivanszhuravlev@gmail.com>
2019-07-01 18:40:54 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+// import StatusBar from './src/components/StatusBar/StatusBar'
+// import UserSettings from './src/pages/UserSettings/UserSettings';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+export default class App extends Component {
+	render() {
+		return (
+			<View>
+				
+			</View>
+    	)
+	}
+}
+
+  // <KeyboardAwareScrollView 
+  //   style={styles.app}
+  //   extraScrollHeight={-80}
+  //   enableOnAndroid={true}
+  // >
+  //   {/* <StatusBar />
+  //   <UserSettings /> */}
+  // </KeyboardAwareScrollView>
---
+++ README.md
@@ -0,0 +1,1 @@
+Meetro Task
